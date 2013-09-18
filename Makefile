1ba588d2c95d843fbe836c1cbf3e3333a40e7753
yy <yy@MacBook-Pro-de-cliente.local>
2013-09-19 01:17:10 +0800

    Integer-Parition added

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 all:
-	cd utils && ./gentex.py > code.tex && xelatex main.tex && xelatex main.tex
+	cd utils && python ./gentex.py > code.tex && xelatex main.tex && xelatex main.tex
 	mv utils/main.pdf ./
 clean:
 	cd utils; rm -f main.pdf main.aux main.log
---
+++ src/6 Damn Math/Integer-Parition.cpp
@@ -0,0 +1,20 @@
+<TeX>
+	整数划分.
+	五边形数 $\frac{3j^{2}-j}{2}$
+	
+	generate function $\prod\limits_{n=1}^{\infty} 1+x^n+x^{2n}+x^{3n}+\dots = \prod\limits_{n=1}^{\infty} \frac{1}{1-x^{n}} $
+
+	$ \prod_{n=1}^\infty (1-x^n)=\sum_{k=-\infty}^\infty(-1)^kx^{k(3k-1)/2}=\sum_{k=0}^\infty(-1)^kx^{k(3k\pm 1)/2}. $
+</TeX>
+
+void parition(int n) {
+    dp[0] = 1;
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1, r = 1; i - (3*j*j-j) / 2 >= 0; ++j, r *= -1) {
+            dp[i] = (dp[i] + dp[i - (3*j*j-j) / 2] * r) % MOD;
+            if (i - (3*j*j + j) / 2 >= 0)
+                dp[i] = (dp[i] + dp[i - (3*j*j+j) / 2] * r) % MOD;
+        }
+    }
+}
+
