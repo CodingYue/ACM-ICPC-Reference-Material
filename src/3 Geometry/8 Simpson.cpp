39361f5b40a2225c843a87ff44a6f6fff539886e
Riatre Foo <Fz258921@Gmail.com>
2013-09-24 20:25:22 +0800

    cubic interpolation added for Simpson.

--- src/3 Geometry/8 Simpson.cpp
+++ src/3 Geometry/8 Simpson.cpp
@@ -1,3 +1,4 @@
+<TeX>如果怀疑被畸形数据了并且时间很多，试试 $$\int_{a}^{b} f(x) \, dx \approx \frac{(b-a)}{8}\left[f(a) + 3f\left(\frac{2a+b}{3}\right) + 3f\left(\frac{a+2b}{3}\right) + f(b)\right]\, $$ </TeX>
 inline double simpson(double fl,double fr,double fmid,double l,double r) { return (fl+fr+4.0*fmid)*(r-l)/6.0; }
 double rsimpson(double slr,double fl,double fr,double fmid,double l,double r)
 {
