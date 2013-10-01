b904df529c4593a36f4d62c7802c44b7f5d8205e
yangyue_cn <this.isssssyy@gmail.com>
2013-10-01 20:17:55 +0800

    .

--- src/3 Geometry/2 Convex Hull.cpp
+++ src/3 Geometry/2 Convex Hull.cpp
@@ -7,13 +7,13 @@ int Graham(Point* P,Point* Hull,int n)
 	for(int i = 0;i < n;i++)
 	{
 		// delete collinear points
-		while(HTop > 1 && valsign((P[i]-Hull[HTop-2])*(Hull[HTop-1]-Hull[HTop-2])) >= 0) HTop--;
+		while(HTop > 1 && dcmp((P[i]-Hull[HTop-2])*(Hull[HTop-1]-Hull[HTop-2])) >= 0) HTop--;
 		Hull[HTop++] = P[i];
 	}
 	int LTop = HTop;
 	for(int i = n-2;i >= 0;i--)
 	{
-		while(HTop > LTop && valsign((P[i]-Hull[HTop-2])*(Hull[HTop-1]-Hull[HTop-2])) >= 0) HTop--;
+		while(HTop > LTop && dcmp((P[i]-Hull[HTop-2])*(Hull[HTop-1]-Hull[HTop-2])) >= 0) HTop--;
 		if(i) Hull[HTop++] = P[i];
 	}
 	return HTop;
