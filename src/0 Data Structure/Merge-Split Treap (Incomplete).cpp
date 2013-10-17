c96d8def63d348e169af2ece3f162bb1b5fc3e89
Riatre Foo <Fz258921@Gmail.com>
2013-10-17 12:48:56 +0800

    修正Treap。

--- src/0 Data Structure/Merge-Split Treap (Incomplete).cpp
+++ src/0 Data Structure/Merge-Split Treap (Incomplete).cpp
@@ -1,33 +1,27 @@
+<TeX>需要改成持久化的话每次修改的时候新建节点即可，然后去掉对fa的维护即可。必要的情况下在newNode里面加上GC。</TeX>
 struct TNODE
 {
-	int val;
-	int rd;
-	int size;
-
-	TNODE* left;
-	TNODE* right;
+	int val,rd,size;
+	TNODE* left,*right,*fa;
 
 	inline int update()
 	{
 		size = 1;
-		if(left) size += left->size;
-		if(right) size += right->size;
+		if(left) { size += left->size; left->fa = this; }
+		if(right) { size += right->size; right->fa = this; }
+		fa = NULL;
 		return 0;
 	}
 };
-
 typedef pair<TNODE*,TNODE*> ptt;
-
-stack<TNODE*> GCPool;
-TNODE TPool[11111111];
+TNODE TPool[233333];
 TNODE* TPTop = TPool;
 
-TNODE* newNode(int val,int rd,TNODE* left,TNODE* right)
+inline int real_rand() { return ((rand()&32767)<<15)^rand(); }
+TNODE* newNode(int val,TNODE* left=NULL,TNODE* right=NULL)
 {
-	TNODE* result = NULL;
-	if(GCPool.size()) { result = GCPool.top(); GCPool.pop(); }
-	else result = TPTop++;
-	result->val = val; result->rd = rd; result->left = left; result->right = right;
+	TNODE* result = TPTop++;
+	result->val = val; result->rd = real_rand(); result->left = left; result->right = right; result->fa = NULL;
 	result->update();
 	return result;
 }
@@ -36,12 +30,10 @@ TNODE* Merge(TNODE* t1,TNODE* t2)
 {
 	if(!t1) return t2;
 	if(!t2) return t1;
-
-	if(t1->rd <= t2->rd) return newNode(t1->val,t1->rd,t1->left,Merge(t1->right,t2));
-	else return newNode(t2->val,t2->rd,Merge(t1,t2->left),t2->right);
+	if(t1->rd <= t2->rd) { t1->right = Merge(t1->right,t2); t1->update(); return t1; }
+	else { t2->left = Merge(t1,t2->left); t2->update(); return t2; }
 }
 
-// split after pos nodes
 ptt Split(TNODE* x,int pos)
 {
 	if(pos == 0) return ptt(NULL,x);
@@ -49,15 +41,35 @@ ptt Split(TNODE* x,int pos)
 
 	int lsize = x->left ? x->left->size : 0;
 	int rsize = x->right ? x->right->size : 0;
-	if(lsize == pos) return ptt(x->left,x->right);
+	if(lsize == pos)
+	{
+		TNODE* oleft = x->left;
+		if(x->left) x->left->update();
+		x->left = NULL;
+		x->update();
+		return ptt(oleft,x);
+	}
 	if(pos < lsize)
 	{
 		ptt st = Split(x->left,pos);
-		return ptt(st.first,newNode(x->val,x->rd,st.second,x->right));
+		x->left = st.second; x->update(); if(st.first) st.first->update();
+		return ptt(st.first,x);
 	}
 	else
 	{
 		ptt st = Split(x->right,pos-lsize-1);
-		return ptt(newNode(x->val,x->rd,x->left,st.first),st.second);
+		x->right = st.first; x->update(); if(st.second) st.second->update();
+		return ptt(x,st.second);
 	}
+}
+
+inline int Rank(TNODE* x)
+{
+	int ans = x->left ? x->left->size : 0;
+	while(x->fa)
+	{
+		if(x == x->fa->right) ans += (x->fa->left ? x->fa->left->size : 0) + 1;
+		x = x->fa;
+	}
+	return ans;
 }
