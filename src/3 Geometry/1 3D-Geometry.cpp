073de1f8e78619c08aa2943b50fb3961ae1788c5
yangyue_cn <this.isssssyy@gmail.com>
2013-10-17 16:38:48 +0800

    fuck

--- src/3 Geometry/1 3D-Geometry.cpp
+++ src/3 Geometry/1 3D-Geometry.cpp
@@ -4,7 +4,7 @@ struct Point {
 	Point(){}
 	Point(double x, double y, double z) : x(x), y(y), z(z) {}
 	Point operator + (const Point &b) { return Point(x + b.x, y + b.y, z + b.z); }
-	Point operator - (const Point &b) { return Point(x - b.x, y - b.y, z - b.y); }
+	Point operator - (const Point &b) { return Point(x - b.x, y - b.y, z - b.z); }
 	Point operator * (const Point &b) { return Point(y * b.z - z * b.y, z * b.x - x * b.z, x * b.y - y * b.x); }
 	Point operator * (const double &b) { return Point(x * b, y * b, z * b); }
 	double operator % (const Point &b) { return x * b.x + y * b.y + z * b.z; }
