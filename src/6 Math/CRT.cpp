5667fafb7dad4c0e731e62ef0fa6f80013f05790
Riatre Foo <Fz258921@Gmail.com>
2013-12-03 01:17:44 +0800

    CRT

---
+++ src/6 Math/CRT.cpp
@@ -0,0 +1,27 @@
+<TeX>lcm是mod的那个数，FACTOR是因子。只保证对于Square-Free Number的正确性。</TeX>
+int ans = 0;
+int lcm = 999911658;
+for(int i = 0;i < 4;i++)
+{
+    int t = lcm/FACTOR[i];
+     
+    int x = 0;
+    int y = 0;
+    int d = 0;
+    exgcd(t,FACTOR[i],d,x,y);
+    x = (ll)(x%lcm+lcm)%lcm*t*R[i]%lcm;
+    ans = (ans+x)%lcm;
+}
+
+int Lucas(int n,int m,int mod)
+{
+    int md = FACTOR[mod];
+    int ans = 1;
+    while(n || m)
+    {
+        ans = ans*_C(n%md,m%md,mod)%md;
+        n /= md;
+        m /= md;
+    }
+    return ans;
+}
