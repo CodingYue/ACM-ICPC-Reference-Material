df992ec9a7de1151c5f97ad9c9c795b16c98c777
Riatre Foo <Fz258921@Gmail.com>
2013-12-05 15:25:06 +0800

    Add something interesting..

---
+++ src/777 外挂/Evaluate.java
@@ -0,0 +1,10 @@
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptEngine;
+public class Main {
+  public static void main(String[] args) throws Exception{
+    ScriptEngineManager mgr = new ScriptEngineManager();
+    ScriptEngine engine = mgr.getEngineByName("JavaScript");
+    String foo = "3+4";
+    System.out.println(engine.eval(foo));
+    }
+}
