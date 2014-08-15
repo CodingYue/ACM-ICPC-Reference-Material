ba1741a5e646f13ceab8102fc58f52fb7c173df8
CodingYue <this.isssssyy@gmail.com>
2014-08-15 19:11:55 +0800

    circle union update

--- src/3 Geometry/9 Circle Union.cpp
+++ src/3 Geometry/9 Circle Union.cpp
@@ -1,5 +1,5 @@
 <TeX>
-	圆的面积并. $area_i$表示恰好被覆盖i次的面积。普通面积并需要去除掉被包含或被内切的圆。getCC() 模板里有。eps 设成$1e-8$ 需要对圆去重.
+	圆的面积并. $area_i$表示恰好被覆盖i次的面积。普通面积并需要去除掉被包含或被内切的圆。eps 设成$1e-8$ 一定要对圆去重.
 </TeX>
 double cal(Point c, double r, double ang1, double ang2) {
 	double ang = ang2 - ang1;
@@ -38,7 +38,7 @@ double solve(int cur, int n) {
 	keys[m++] = make_pair(0, 0);
 	keys[m++] = make_pair(2 * pi, - 100000);
 	sort(keys, keys + m, cmp);
-	double res = cal(cir[cur].c, cir[cur].r, 0, keys[0].first);
+	double res = 0;
 	int cnt = 0;
 	for (int i = 0; i < m; ++i) {
 		cnt += keys[i].second;
