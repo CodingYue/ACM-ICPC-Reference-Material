b01c941184d7e8cfcb19a7a0f115f8496feb4f48
Flydutchman <flydutchman@foxmail.com>
2014-10-03 21:14:12 +0800

    添加了两种lct（路径、子树）和一个比较朴素（经典?）的消圈算法。

---
+++ src/0 Data Structure/LCT_Path.cpp
@@ -0,0 +1,101 @@
+// Link-Cut-Tree模板，支持路径查询、路径修改、link、cut、不支持子树操作。
+//MaxNode 节点数
+//用的时候在外面对输入树的每个节点i调用一下pt[i].Init()即可。
+struct node {
+    int v, f, ch[2], s;
+    node() {}
+    void Init(int _v, int _f) {
+        v = _v; f = _f;
+        ch[0] = ch[1] = 0;
+        s = 1;
+    }
+}pt[MaxNode];
+
+struct LCT {
+    struct node{
+        int f,ch[2],sum,v,s; bool rev,isroot;
+        void Make_Rev() {
+            rev = !rev; swap(ch[0],ch[1]);
+        }
+        void Init(int _v, int _f) {
+            f = _f; ch[0] = ch[1] = 0; isroot = true; rev = false;
+            sum = v = _v;
+            s = 1;
+        }
+    }pt[MaxNode];
+    void push(int t) {
+        if (pt[t].rev) {
+            if (pt[t].ch[0]) pt[pt[t].ch[0]].Make_Rev();
+            if (pt[t].ch[1]) pt[pt[t].ch[1]].Make_Rev();
+            pt[t].rev = false;
+        }
+    }
+    void updata(int t) {
+        pt[t].s = pt[pt[t].ch[0]].s + pt[pt[t].ch[1]].s + 1;
+        pt[t].sum = pt[pt[t].ch[0]].sum + pt[pt[t].ch[1]].sum + pt[t].v;
+    }
+    void zig(int x,bool w) {
+        int y = pt[x].f; if (pt[y].isroot) { pt[y].isroot = false; pt[x].isroot = true; }
+        push(y); push(x);
+        pt[y].ch[!w] = pt[x].ch[w]; if (pt[x].ch[w]) pt[pt[x].ch[w]].f = y;
+        pt[x].f = pt[y].f; if (!pt[x].isroot) pt[pt[y].f].ch[y == pt[pt[y].f].ch[1]] = x;
+        pt[x].ch[w] = y; pt[y].f = x; updata(y);
+    }
+    void splay(int x) {
+        while (!pt[x].isroot) {
+            push(x);
+            if (pt[pt[x].f].isroot) zig(x, x == pt[pt[x].f].ch[0]);
+            else { 
+                int y = pt[x].f, z = pt[y].f;
+                if (y == pt[z].ch[0]) if (x == pt[y].ch[0]) {zig(y,1); zig(x,1); } else {zig(x,0); zig(x,1); }
+                    else if (x == pt[y].ch[0]) { zig(x,1); zig(x,0); } else {zig(y,0); zig(x,0); }
+            }
+        } updata(x);
+    }
+    int  access(int x,int cas) { // -1 for reverse
+        int p = 0, q = x, ret = 0;
+        while (q) {
+            splay(q); push(q);
+            if (pt[q].f == 0) {
+                ret = pt[pt[q].ch[1]].sum + pt[p].sum;
+                if (pt[q].ch[1]) {
+                    int t = pt[q].ch[1];    
+                    if (!p) {
+                        while (pt[t].ch[0]) t = pt[t].ch[0];
+                        ret -= pt[t].v;
+                    }
+                }
+                if (p) {
+                    int t = p;
+                    if (!pt[q].ch[1]) {
+                        while (pt[t].ch[0]) t = pt[t].ch[0];
+                        ret -= pt[t].v;
+                    }
+                }
+            }
+            pt[pt[q].ch[1]].isroot = true; pt[p].isroot = false;
+            pt[q].ch[1] = p; updata(q); p = q; q = pt[q].f;
+        }
+        splay(p);
+        if (cas == 0) ret = pt[p].s;
+        if (cas == -1) pt[p].Make_Rev();
+        return ret;
+    }
+    int root(int x) {
+        access(x,0);
+        splay(x);
+        while (pt[x].ch[0]) x = pt[x].ch[0];
+        return x;
+    }
+    void Link(int x,int y) {
+        access(x, -1); splay(x); pt[x].f = y;
+    }
+    bool Son(int x,int y) {
+        access(x,0); splay(y); 
+        return pt[y].ch[1] == x;
+    }  
+    void Cut(int x,int y) {
+        if (Son(x,y)) swap(x,y);
+        access(y,0); splay(x); pt[y].isroot = true; pt[x].ch[1] = 0; pt[y].f = 0;
+    }
+}Tree;
---
+++ src/0 Data Structure/LCT_Subtree.cpp
@@ -0,0 +1,92 @@
+// MaxN 节点数
+// 支持link cut 修改点权，查询所在联通块最大值
+// 简单版的维护子树lct，不要随便改奇怪的地方。
+
+struct node {
+    int f,ch[2],Max; bool isroot,rev;
+    multiset<int> Heap;
+    void Init(int _v,int _f) {
+        isroot = true; rev = false ;
+        ch[0] = ch[1] = 0; f = _f;
+        Heap.clear(); Heap.insert(_v); Max = _v;
+    }
+    void Make_Rev() {
+        swap(ch[0],ch[1]); rev ^= 1;
+    }    
+}pt[MaxN];
+
+struct LCT {
+    void push(int t) {
+        if (pt[t].rev) {
+            if (pt[t].ch[0]) pt[pt[t].ch[0]].Make_Rev();
+            if (pt[t].ch[1]) pt[pt[t].ch[1]].Make_Rev();
+            pt[t].rev = 0;
+        }
+    }
+    void update(int t) {
+        if (!t) while(1);
+        pt[t].Max = max(*pt[t].Heap.rbegin(),max(pt[pt[t].ch[0]].Max,pt[pt[t].ch[1]].Max));
+    }
+    void zig(int x,bool w){
+        int y = pt[x].f; push(y); push(x); 
+        if(pt[y].isroot) {pt[y].isroot = false; pt[x].isroot = true; }
+        push(y); push(x);
+        pt[y].ch[!w] = pt[x].ch[w]; if(pt[x].ch[w]) pt[pt[x].ch[w]].f = y;
+        pt[x].f = pt[y].f; if(!pt[x].isroot) pt[pt[y].f].ch[y == pt[pt[y].f].ch[1]] = x;
+        pt[x].ch[w] = y; pt[y].f = x; update(y);
+    }
+    void splay(int x){
+        while(!pt[x].isroot){
+            push(x);
+            if(pt[pt[x].f].isroot) zig(x,x == pt[pt[x].f].ch[0]);
+            else { int y = pt[x].f, z = pt[y].f;
+                if(y == pt[z].ch[0])
+                    if(x == pt[y].ch[0]){ zig(y,1); zig(x,1); }
+                    else { zig(x,0); zig(x,1); }
+                else if(x == pt[y].ch[0]){ zig(x,1); zig(x,0); }
+                    else { zig(y,0); zig(x,0); }
+            }
+        }
+        update(x);
+    }
+    int access(int x){//1 询问 2 Reverse 0 没事 
+        int p = 0,q = x; PII ret = MP(0,0);
+        while (q) {
+            splay(q); push(q);
+            if (pt[q].ch[1]) {
+                pt[pt[q].ch[1]].isroot = true; pt[q].Heap.insert(pt[pt[q].ch[1]].Max);
+            }
+            if (p) {
+                if (pt[q].Heap.find(pt[p].Max) == pt[q].Heap.end()) {
+                    int z = q;
+                }
+                pt[p].isroot = false; pt[q].Heap.erase(pt[q].Heap.find(pt[p].Max));
+            }
+            pt[q].ch[1] = p; update(q);
+            p = q; q = pt[q].f;
+        }
+        return 0;
+    }
+    int Find(int p){
+        access(p);splay(p);
+        int t = p; while(pt[t].ch[0]) t = pt[t].ch[0];
+        return t;
+    }
+    void Link(int x,int y){//x儿子 y父亲
+        if(x==y)return;
+        access(x); access(y);
+        pt[x].f = y; pt[y].Heap.insert(pt[x].Max);
+        update(y);
+    }
+    bool Son(int x,int y){//返回x是y的儿子
+        access(y); splay(x); return (pt[x].f == y);
+    }
+    void Cut(int x,int y){
+        if(!Son(x,y)) swap(x,y);
+        if(x==y)return;
+        //pt[y].Heap.erase(pt[y].Heap.find(pt[x].Max));
+        access(x); splay(y);
+        pt[y].ch[1] = 0; update(y);
+        pt[x].f = 0; pt[x].isroot = true;
+    }
+}DTree;
