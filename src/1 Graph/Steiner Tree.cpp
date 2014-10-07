2ba902afce68ac322e62c135a21c986ebec8158c
Riatre Foo <Fz258921@Gmail.com>
2014-10-07 11:07:05 +0800

    整理 & 添加更多chota1219中的内容。

---
+++ src/1 Graph/Steiner Tree.cpp
@@ -0,0 +1,31 @@
+<TeX>求非负无向图上包含ts中所有点的最小Steiner树。G是邻接矩阵。dp[S][v]表示包含S中的点和v的最小Steiner树。 $ O(3^tn+2^tn^2+n^3) $</TeX>
+
+int dp[1 << MAX_M][MAX_N]; // no memset needed
+int steiner(int n, vector<int> ts)
+{
+	int m = ts.size();
+	if(m < 2) return 0;
+
+	for(int k = 0;k < n;k++) // Floyd-Warshall
+		for(int i = 0;i < n;i++)
+			for(int j = 0;j < n;j++)
+				G[i][j] = min(G[i][j],G[i][k] + G[k][j]);
+
+	for(int i = 0;i < m;i++)
+		for(int j = 0;j < n;j++)
+			dp[1 << i][j] = G[ts[i]][j];
+	
+	for(int i = 1;i < (1 << m);i++) if( ((i-1) & i) != 0 )
+	{
+		for(int j = 0;j < n;j++)
+		{
+			dp[i][j] = INF; // 0x3F3F3F3F or something like.
+			for(int k = (i-1) & i;k > 0;k = (k-1) & i)
+				dp[i][j] = min(dp[i][j], dp[k][j] + dp[i^k][j]);
+		}
+		for(int j = 0;j < n;j++)
+			for(int k = 0;k < n;k++)
+				dp[i][j] = min(dp[i][j], dp[i][k] + G[k][j]);
+	}
+	return dp[(1<<m) - 1][ts[0]];
+}
---
+++ src/6 Math/Recurrence Expansion.cpp
@@ -0,0 +1,35 @@
+<TeX>治疗递推式。对于形如$a_{i+n} = \sum k_ja_{i+j} + d$的递推式，求出其写成$a_m = \sum c_ia_i + c_nd$这一形式时的系数$c_i$。工作在double上，可以比较显然的改成int取模。
+代码中$c$为传出的答案，需要自行alloc。
+
+$O(n^2 \log m)$ $k_i$稀疏的话用FFT可以达成$O(n \log n \log m)$</TeX>
+
+// c at least double c[n+1]
+int recFormula(double* k, int n, long long m, double* c)
+{
+	memset(c,0,sizeof(*c)*(n+1));
+	if(m < n) c[m] = 1;
+	else
+	{
+		double* b = new double[n+1];
+		recFormula(k, n, m >> 1, b);
+
+		static double a[MAXN*2];
+		memset(a,0,sizeof(a[0])*(n*2));
+
+		int s = m & 1;
+		for(int i = 0;i < n;i++)
+		{
+			for(int j = 0;j < n;j++) a[i + j + s] += b[i] * b[j];
+			c[n] += b[i];
+		}
+		c[n] = (c[n] + 1) * b[n];
+		for(int i = n * 2 - 1;i >= n;i--)
+		{
+			for(int j = 0;j < n;j++) a[i - n + j] += k[j] * a[i];
+			c[n] += a[i];
+		}
+		memcpy(c,a,sizeof(c[0])*n); // c[n] is c[n], do not copy it.
+		delete[] b;
+	}
+	return 0;
+}
